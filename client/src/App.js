9646039a37ed0ff3e778b499aa1c7e6158a9002c
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-03-31 12:55:34 -0400

    added gql queries

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
 
 function App() {
   return (
@@ -18,5 +20,17 @@ function App() {
     </Router>
   );
 }
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+})
 
 export default App;
