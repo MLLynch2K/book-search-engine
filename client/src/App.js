a09e33130ca90404378f21bf44469780069f33c0
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-03-31 13:23:31 -0400

    fixed apollo boost errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks'
-import ApolloClient from 'apollo-boost'
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// added apollo client
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
 const client = new ApolloClient({
   request: operation => {
     const token = localStorage.getItem('id_token')
